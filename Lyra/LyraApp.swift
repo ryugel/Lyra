86263579c1db2f36cfe45f25b0823515e8f3d1f8
Augustin Diabira <augustindiabira@gmail.com>
2024-09-21 19:48:26 +0200

    Initial Commit

---
+++ Lyra/LyraApp.swift
@@ -0,0 +1,19 @@
+//
+//  LyraApp.swift
+//  Lyra
+//
+//  Created by ryugel on 21/09/2024.
+//  Copyright © 2024 DeRosa. All rights reserved.
+//
+       
+
+import SwiftUI
+
+@main
+struct LyraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LyraTests/LyraTests.swift
@@ -0,0 +1,18 @@
+//
+//  LyraTests.swift
+//  Lyra
+//
+//  Created by ryugel on 21/09/2024.
+//  Copyright © 2024 DeRosa. All rights reserved.
+//
+       
+
+import Testing
+
+struct LyraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
